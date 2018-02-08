aa6b7b2205b7491071a89e4f9da9967578778a1a
Naterra <natalinyweb@hotmail.com>
2018-02-07 21:00:07 -0500

    Vowels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -31,27 +31,26 @@
 //     }
 // }
 
-
 function pyramid(n, row = 0, level = '') {
-    if(row === n){
-        return;
-    }
+	if (row === n) {
+		return;
+	}
 
-    // If level.length is last
-    if(level.length === 2 * n - 1){
-        console.log(level);
-        return pyramid(n, row + 1 );
-    }
+	// If level.length is last
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		return pyramid(n, row + 1);
+	}
 
-    const midpoint = Math.floor((2 * n - 1) / 2);
+	const midpoint = Math.floor((2 * n - 1) / 2);
 
-    if(midpoint - row <= level.length && midpoint + row >= level.length){
-        add = '#';
-    }else{
-        add = ' ';
-    }
+	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	}
 
-    pyramid(n, row, level + add);
+	pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
